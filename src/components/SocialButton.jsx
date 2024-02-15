9590cd10e4aa037752d7cd13c0c15075b8076cac
GustavoPG <gustavoparadag66@gmail.com>
2024-02-15 11:23:20 -0300

    create components files

---
+++ src/components/SocialButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../assets/css/socialButton.css'
+
+const SocialButton = ({ links }) => {
+    //console.log(namesIcon);
+    return (
+        <div className='my-icons'>
+            {
+                links.map((link, index) =>
+
+                    <a key={index} href={link.url} className="my-icon" target="_blank">
+                        <i className={link.iconName}></i>
+                    </a>
+
+                )
+            }
+        </div>
+    );
+};
+
+export default SocialButton
